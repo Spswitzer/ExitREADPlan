811ae230aa6f391f6d241442bd7dcaab7b5d0c72
spswitzer <susan.switzer@jeffco.k12.co.us>
2024-10-14 10:19:26 -0600

    Refine query

--- readPlanReview.R
+++ readPlanReview.R
@@ -27,30 +27,39 @@ qrystudentPlan <- odbc::dbGetQuery(con,
    ,tStudentNeed.FirstName
    ,tStudentNeed.LastName
    ,tStudentNeed.PersonID
-   ,tStudentNeed.NeedTypeID
    ,tStudentNeed.ConcernStartDate
    ,tStudentNeed.LastUpdatedDate
-   ,tStudentNeed.ActiveFlag
+   ,tStudentNeed.ActiveFlag -- Not sure what this indicates
    ,tStudentNeedFocusArea.StudentNeedFocusAreaID
+    ,tFocusArea.FocusAreaName
+  -- ,tGoal.SmartGoal
    ,tStudentNeedFocusArea.FocusAreaID
---  ,tGoal.SmartGoal
- --  ,tGoal.ProgressMonitoring
-  -- ,tGoal.Frequency
+  -- ,tGoal.ProgressMonitoring --error: ODBC SQL Server Driver]Invalid Descriptor Index 
   FROM
     dbSOARS.rti.tStudentNeed (NOLOCK)
   JOIN dbSOARS.rti.tStudentNeedFocusArea (NOLOCK) ON
     tStudentNeedFocusArea.StudentNeedID = tStudentNeed.StudentNeedID
-  --JOIN dbSOARS.rti.tGoal (NOLOCK) ON
- --   tGoal.StudentNeedID = tStudentNeed.StudentNeedID
+  JOIN dbSOARS.rti.tGoal (NOLOCK) ON
+    tGoal.StudentNeedID = tStudentNeed.StudentNeedID
  JOIN dbSOARS.rti.tNeedType (NOLOCK) ON
- tNeedType.NeedTypeID = tStudentNeed.NeedTypeID
+   tNeedType.NeedTypeID = tStudentNeed.NeedTypeID
+  JOIN dbSOARS.rti.tFocusArea (NOLOCK) ON
+   tFocusArea.FocusAreaID = tStudentNeedFocusArea.FocusAreaID
+  --JOIN dbSOARS.rti.tInstruction (NOLOCK) ON
   WHERE
     tNeedType.NeedTypeName = 'READ'
   AND
     tStudentNeed.lastUpdatedDate > '2023-07-01'
   AND 
     tstudentNeed.ConcernStartDate < '2024-07-01'
+  AND 
+    tStudentNeed.ActiveFlag = 1
   ORDER BY
     tStudentNeed.ConcernStartDate
 "
 )
+
+
+focusAreas <- qrystudentPlan %>% 
+  right_join(flagWider, join_by(PersonID == personID))
+
