7a60c7a5c610151b45d916ff58eb6bec3582e362
spswitzer <susan.switzer@jeffco.k12.co.us>
2024-10-14 09:44:33 -0600

    Initial

---
+++ readPlanReview.R
@@ -0,0 +1,56 @@
+# # Script header ----
+# Title: READ Plan documents
+# Author: Susan Switzer
+# Created: 10/14/24
+# Revised: 
+# The purpose of this script is to review READ Plan elements for 3rd students (2023-2024) from the kindergarten class of 2020-2021 
+
+#load libraries ----
+library(odbc)
+library(DBI)
+library(janitor)
+library(tidyverse)
+# connect to SQL database ----
+con <- dbConnect(odbc(),
+                 Driver = "SQL Server",
+                 Server = "qdc-soars-test",
+                 trusted_connection = "true",
+                 Port = 1433)
+
+sort(unique(odbcListDrivers()[[1]]))
+
+# Query of READ Plan Flag in Campus ----
+qrystudentPlan <- odbc::dbGetQuery(con, 
+"
+  SELECT
+   tStudentNeed.studentNeedID
+   ,tStudentNeed.FirstName
+   ,tStudentNeed.LastName
+   ,tStudentNeed.PersonID
+   ,tStudentNeed.NeedTypeID
+   ,tStudentNeed.ConcernStartDate
+   ,tStudentNeed.LastUpdatedDate
+   ,tStudentNeed.ActiveFlag
+   ,tStudentNeedFocusArea.StudentNeedFocusAreaID
+   ,tStudentNeedFocusArea.FocusAreaID
+--  ,tGoal.SmartGoal
+ --  ,tGoal.ProgressMonitoring
+  -- ,tGoal.Frequency
+  FROM
+    dbSOARS.rti.tStudentNeed (NOLOCK)
+  JOIN dbSOARS.rti.tStudentNeedFocusArea (NOLOCK) ON
+    tStudentNeedFocusArea.StudentNeedID = tStudentNeed.StudentNeedID
+  --JOIN dbSOARS.rti.tGoal (NOLOCK) ON
+ --   tGoal.StudentNeedID = tStudentNeed.StudentNeedID
+ JOIN dbSOARS.rti.tNeedType (NOLOCK) ON
+ tNeedType.NeedTypeID = tStudentNeed.NeedTypeID
+  WHERE
+    tNeedType.NeedTypeName = 'READ'
+  AND
+    tStudentNeed.lastUpdatedDate > '2023-07-01'
+  AND 
+    tstudentNeed.ConcernStartDate < '2024-07-01'
+  ORDER BY
+    tStudentNeed.ConcernStartDate
+"
+)
