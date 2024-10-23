069c69a58e9795ed90ad06e9ea96a5646148a6d1
spswitzer <susan.switzer@jeffco.k12.co.us>
2024-10-23 07:55:04 -0600

    Update Query to include goal

--- readPlanReview.R
+++ readPlanReview.R
@@ -22,20 +22,17 @@ sort(unique(odbcListDrivers()[[1]]))
 # Query of READ Plan Flag in Campus ----
 qrystudentPlan <- odbc::dbGetQuery(con, 
 "
-  SELECT
+  SELECT DISTINCT
    tStudentNeed.studentNeedID
    ,tStudentNeed.FirstName
    ,tStudentNeed.LastName
    ,tStudentNeed.PersonID
    ,tStudentNeed.ConcernStartDate
    ,tStudentNeed.LastUpdatedDate
+   --,tStudentNeed.NeedDescription --empty
+   --,tStudentNeed.isDisplayedStaffDashboard --FALSE for sample student
+   --,tStudentNeed.isDisplayedStudentParentDashboard  --FALSE for sample student
    ,tStudentNeed.ActiveFlag -- Not sure what this indicates
-   ,tInstruction.ActiveFlag AS InstFlag -- Not sure what this indicates
-   ,tGoal.ActiveFlag AS GoalFlag -- Not sure what this indicates
-   ,tFocusAreaDesignator.ActiveFlag AS FocusFlag -- Not sure what this indicates
-   ,tGoalInstruction.ActiveFlag AS goalInstFlag -- Not sure what this indicates
-   ,tNeedType.ActiveFlag AS needFlag -- Not sure what this indicates
-   ,tStudentNeedFocusArea.ActiveFlag AS focusAreaFlag -- Not sure what this indicates
    ,tInstruction.ActiveFlag AS InstFlag -- Not sure what this indicates
    ,tGoal.ActiveFlag AS GoalFlag -- Not sure what this indicates
    ,tFocusAreaDesignator.ActiveFlag AS FocusFlag -- Not sure what this indicates
@@ -44,19 +41,21 @@ qrystudentPlan <- odbc::dbGetQuery(con,
    ,tStudentNeedFocusArea.ActiveFlag AS focusAreaFlag -- Not sure what this indicates
    ,tStudentNeedFocusArea.StudentNeedFocusAreaID
    ,tFocusArea.FocusAreaName
-   ,tFocusArea.FocusAreaName
-  -- ,tGoal.SmartGoal
-  -- ,tGoal.ProgressMonitoring --error: ODBC SQL Server Driver]Invalid Descriptor Index 
+   --,tFocusArea.StartYear --Not working 1900
+   --,tFocusArea.EndYear --Not working 2099
+   --,tGoal.ParentAcknowledgeDate -- NA for sample student
+   -- ,tGoal.ProgressMonitoring --error: ODBC SQL Server Driver]Invalid Descriptor Index 
    ,tStudentNeedFocusArea.FocusAreaID
-  --,tStudentNeedFocusArea.FocusAreaRanking
+   --,tStudentNeedFocusArea.FocusAreaRanking
    ,tStudentNeedFocusArea.StartDate AS SNStart
    ,tStudentNeedFocusArea.EndDate AS SNEnd
-  -- ,tInstruction.InstructionDescription
-  -- ,tInstruction.ImplementedDate
-  -- ,tInstruction.CompletedDate
-  -- ,tInstruction.InstructionMinutes
-  -- ,tInstruction.TimesPerWeek
-   ,tFocusAreaDesignator.DesignatorID
+   ,tInstruction.InstructionDescription
+  ,tInstruction.ImplementedDate
+  ,tInstruction.CompletedDate
+  ,tInstruction.InstructionMinutes
+  ,tInstruction.TimesPerWeek
+   --,tFocusAreaDesignator.DesignatorID
+   ,tGoal.SmartGoal --must be at end of query
 FROM
     dbSOARS.rti.tStudentNeed (NOLOCK)
   JOIN dbSOARS.rti.tStudentNeedFocusArea (NOLOCK) ON
@@ -67,13 +66,13 @@ FROM
    tNeedType.NeedTypeID = tStudentNeed.NeedTypeID
   JOIN dbSOARS.rti.tFocusArea (NOLOCK) ON
    tFocusArea.FocusAreaID = tStudentNeedFocusArea.FocusAreaID
-  --JOIN dbSOARS.rti.tInstruction (NOLOCK) ON
   JOIN dbSOARS.rti.tGoalInstruction (NOLOCK) ON
     tGoalInstruction.GoalID = tGoal.GoalID
   JOIN dbSOARS.rti.tGoalFocusArea (NOLOCK) ON
     tGoalFocusArea.GoalID = tGoal.GoalID
   JOIN dbSoARS.rti.tInstruction (NOLOCK) ON
-    tInstruction.InstructionID = tGoalInstruction.InstructionID
+    tInstruction.InstructionID = tGoalInstruction.InstructionID AND
+     tInstruction.studentNeedID = tStudentNeed.studentNeedID
   JOIN dbSOARS.rti.tFocusAreaDesignator (NOLOCK) ON
     tFocusAreaDesignator.FocusAreaID =  tStudentNeedFocusArea.FocusAreaID
 WHERE
@@ -84,7 +83,10 @@ WHERE
     tstudentNeed.ConcernStartDate < '2024-07-01'
   AND 
     tStudentNeed.ActiveFlag = 1
+ -- AND 
+    --tStudentNeed.PersonID = 1647360
   ORDER BY
     tStudentNeed.ConcernStartDate
 "
 )
+
