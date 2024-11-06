f03f671f0e6dbab1854d223f5cfdf4af07d1d6d7
spswitzer <susan.switzer@jeffco.k12.co.us>
2024-11-06 09:21:40 -0700

    Update for all years of interest

--- readPlanStateSubmission.R
+++ readPlanStateSubmission.R
@@ -2,7 +2,7 @@
 # Title: READ Plan State Submission
 # Author: Susan Switzer
 # Created: 10/14/24
-# Revised: 10/25/24
+# Revised: 11/06/24
 # The purpose of this script is to access READ Plan data subimission in 2024
 
 #load libraries ----
@@ -28,77 +28,146 @@ qryREADPlanSubmission <- odbc::dbGetQuery(con,
   FROM
     dbSOARS.readplan.tREADSubmission (NOLOCK) 
   WHERE
-    tREADSubmission.EndYear = 2024
+    tREADSubmission.EndYear BETWEEN 2021 AND 2024
 "
 )
 
 # Remove records noted as excluded ----
 readCleaned <- qryREADPlanSubmission %>% 
-  filter(is.na(Excluded)) %>% 
-  filter(GradeID < 4)
+  clean_names('lower_camel') %>% 
+  filter(is.na(excluded)) %>% 
+  filter(gradeId < 4)
+
+#Submission Summary All Cohorts ----
+allCohorts <- readCleaned %>% 
+  group_by(endYear, gradeId) %>% 
+  mutate(gradeN = n()) %>% 
+  group_by(endYear, gradeId, interventionServicesRead) %>% 
+  summarise(gradeN = first(gradeN), 
+            readN = n(), 
+            readPct = readN/gradeN)
+  
+
+allCohortsAllGrades <- readCleaned %>% 
+  group_by(endYear) %>% 
+  mutate(totalN = n()) %>% 
+  group_by(endYear, interventionServicesRead) %>% 
+  summarise(totalN = first(totalN), 
+            readN = n(), 
+            readPct = readN/totalN)
+
+library(gt)
+
+planByYear <- function(.endyear = 2021) {
+  gradesTable <- allCohorts %>% 
+    group_by(endYear) %>% 
+    filter(interventionServicesRead == 1, 
+           endYear == .endyear) %>% 
+    group_by(gradeId) %>% 
+    mutate(gradeId = factor(gradeId, 
+                            levels = 0:3, 
+                            labels = c('Kindergarten', 'Grade 1', 'Grade 2', 'Grade 3'))) %>% 
+    gt() %>% 
+    cols_label(gradeN = 'N in Grade Level', 
+               readN = 'N with READ Services', 
+               readPct = 'Percent') %>% 
+    cols_hide(c(endYear, interventionServicesRead)) %>% 
+    fmt_number(c(gradeN, readN), decimals = 0) %>% 
+    fmt_percent(readPct, decimals = 0) %>% 
+    opt_table_outline() %>% 
+    tab_style(
+      cell_fill('grey'), 
+      cells_row_groups()
+    ) %>% 
+    tab_options(
+      table.font.size = 12
+    ) %>% 
+    tab_header(
+      title = 'Student Plan starting season', 
+      subtitle = paste0('Grade 3 in ' , .endyear)
+    )
+  return(gradesTable)
+}
+
+planByYear(2024)
 
 ## Summarise services students recieved for SRD ----
-readServices <- readCleaned %>% 
-  pivot_longer(cols = c(SummerSchoolRead, TutorRead, InterventionServicesREAD),
-               names_to = 'service',
-               values_to = 'serviceValue') %>%
-  group_by(GradeID) %>% 
-  mutate(n = n_distinct(personID)) %>% 
-  group_by(ReadPlan, GradeID) %>% 
-  mutate(readPlanN = n_distinct(personID)) %>% 
-  group_by(GradeID, service, serviceValue) %>% 
-  summarise(servicesN = n_distinct(personID), 
-            n = first(n), 
-            servicesPct = round(servicesN/n, 4),
-            readPlanN = first(readPlanN),
-            readPct = round(readPlanN/n, 4)) %>% 
-  filter(serviceValue == 1)
+# readServices <- readCleaned %>% 
+#   pivot_longer(cols = c(SummerSchoolRead, TutorRead, InterventionServicesREAD),
+#                names_to = 'service',
+#                values_to = 'serviceValue') %>%
+#   group_by(GradeID) %>% 
+#   mutate(n = n_distinct(personID)) %>% 
+#   group_by(ReadPlan, GradeID) %>% 
+#   mutate(readPlanN = n_distinct(personID)) %>% 
+#   group_by(GradeID, service, serviceValue) %>% 
+#   summarise(servicesN = n_distinct(personID), 
+#             n = first(n), 
+#             servicesPct = round(servicesN/n, 4),
+#             readPlanN = first(readPlanN),
+#             readPct = round(readPlanN/n, 4)) %>% 
+#   filter(serviceValue == 1)
 
 ## Summarize N and Percent of students at each school and grade level with READ Plan ----
-readSchools <- readCleaned %>% 
-  select(personID, CDESchoolCode, GradeID, ReadPlan) %>% 
-  group_by(CDESchoolCode) %>% 
-  mutate(schoolN = n()) %>% 
-  group_by(CDESchoolCode, GradeID) %>% 
-  mutate(gradeN = n()) %>% 
-  group_by(CDESchoolCode, GradeID, ReadPlan) %>% 
-  reframe(readPlanN = n(),
-            gradeN = first(gradeN), 
-            gradePct = round(readPlanN/gradeN, 4)
-          ) %>% 
-  filter(ReadPlan == 1)
-
-## Summarize READ Act interim assessment administered used for determine SRD status ----
-### Create look up table of assessments based on https://www.cde.state.co.us/coloradoliteracy/22-23springreadassessmentfilelayoutanddefinitions ---
-readTestName <- data.frame(ReadTest = c('04', '19', '13', 
-                                        '20', '06', '10'), 
-                           TestName = c('Exemption NEP', 'DIBELS8', 'Alternative Assessment', 
-                                        'Lectura', 'Exemption Attendance', 'iReady'))
-
-readTest <- readCleaned %>% 
-  select(personID, GradeID, ReadTest) %>% 
-  group_by(GradeID) %>% 
-  mutate(gradeN = n()) %>% 
-  group_by(ReadTest, GradeID) %>% 
-  reframe(n = n(), 
-         gradeN = first(gradeN)) %>% 
-  full_join(readTestName)
-
-readTestGender <- readCleaned %>% 
-  select(personID, GradeID, Gender, ReadPlan) %>% 
-  group_by(GradeID, ReadPlan) %>% 
-  mutate(gradeN = n()) %>% 
-  group_by(Gender, GradeID, ReadPlan) %>% 
-  reframe(n = n(), 
-          gradeN = first(gradeN)) %>% 
-  filter(ReadPlan == 1) %>% 
-  mutate(genderPct = n/gradeN) %>% 
-  mutate(genderName = case_when(
-    Gender == '01' ~ 'female', 
-    Gender == '02' ~ 'male', 
-    TRUE ~ 'gender expansive'
-  ))
-
+# readSchools <- readCleaned %>% 
+#   select(personID, CDESchoolCode, GradeID, ReadPlan) %>% 
+#   group_by(CDESchoolCode) %>% 
+#   mutate(schoolN = n()) %>% 
+#   group_by(CDESchoolCode, GradeID) %>% 
+#   mutate(gradeN = n()) %>% 
+#   group_by(CDESchoolCode, GradeID, ReadPlan) %>% 
+#   reframe(readPlanN = n(),
+#             gradeN = first(gradeN), 
+#             gradePct = round(readPlanN/gradeN, 4)
+#           ) %>% 
+#   filter(ReadPlan == 1)
+# 
+# ## Summarize READ Act interim assessment administered used for determine SRD status ----
+# ### Create look up table of assessments based on https://www.cde.state.co.us/coloradoliteracy/22-23springreadassessmentfilelayoutanddefinitions ---
+# readTestName <- data.frame(ReadTest = c('04', '19', '13', 
+#                                         '20', '06', '10'), 
+#                            TestName = c('Exemption NEP', 'DIBELS8', 'Alternative Assessment', 
+#                                         'Lectura', 'Exemption Attendance', 'iReady'))
+# 
+# readTest <- readCleaned %>% 
+#   select(personID, GradeID, ReadTest) %>% 
+#   group_by(GradeID) %>% 
+#   mutate(gradeN = n()) %>% 
+#   group_by(ReadTest, GradeID) %>% 
+#   reframe(n = n(), 
+#          gradeN = first(gradeN)) %>% 
+#   full_join(readTestName)
+# 
+# readTestGender <- readCleaned %>% 
+#   select(personID, GradeID, Gender, ReadPlan) %>% 
+#   group_by(GradeID, ReadPlan) %>% 
+#   mutate(gradeN = n()) %>% 
+#   group_by(Gender, GradeID, ReadPlan) %>% 
+#   reframe(n = n(), 
+#           gradeN = first(gradeN)) %>% 
+#   filter(ReadPlan == 1) %>% 
+#   mutate(genderPct = n/gradeN) %>% 
+#   mutate(genderName = case_when(
+#     Gender == '01' ~ 'female', 
+#     Gender == '02' ~ 'male', 
+#     TRUE ~ 'gender expansive'
+#   ))
+# readServices <- readCleaned %>% 
+#     pivot_longer(cols = c(SummerSchoolRead, TutorRead, InterventionServicesREAD),
+#                  names_to = 'service',
+#                  values_to = 'serviceValue') %>%
+#     group_by(GradeID) %>%
+#     mutate(n = n_distinct(personID)) %>%
+#     group_by(ReadPlan, GradeID) %>%
+#     mutate(readPlanN = n_distinct(personID)) %>%
+#     group_by(GradeID, service, serviceValue) %>%
+#     summarise(servicesN = n_distinct(personID),
+#               n = first(n),
+#               servicesPct = round(servicesN/n, 4),
+#               readPlanN = first(readPlanN),
+#               readPct = round(readPlanN/n, 4)) %>%
+#     filter(serviceValue == 1)
+  
 nOfPlans <- readCleaned %>% 
   group_by(GradeID) %>% 
   mutate(gradeTotalN = n()) %>% 
